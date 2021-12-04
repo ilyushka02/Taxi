6da2cf3db13fadce503a65e56f78866fdbd3d30e
Ilya Nasurllaev <ilyaevgenievi4@mail.ru>
2021-12-04 23:55:59 +0300

    add activity for registration

--- app/src/main/java/com/example/taxi/SignInActivity.java
+++ app/src/main/java/com/example/taxi/SignInActivity.java
@@ -2,14 +2,39 @@ package com.example.taxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_in);
+        TextView textView_createAcc = findViewById(R.id.textCreateNewAccount);
+        textView_createAcc.setOnClickListener(this);
+        Button button_signIN =findViewById(R.id.button_sigIn);
+        button_signIN.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_sigIn:
+//                openMaps();
+                break;
+            case R.id.textCreateNewAccount:
+                openSignup();
+                break;
+        }
+    }
+
+    private void openSignup() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
