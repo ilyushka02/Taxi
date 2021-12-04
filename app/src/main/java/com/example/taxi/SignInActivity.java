0b57a3418e236087293b1448a0a51e02380665c0
Ilya Nasurllaev <ilyaevgenievi4@mail.ru>
2021-12-04 15:11:25 +0300

    create splash for app

--- app/src/main/java/com/example/taxi/SignInActivity.java
+++ app/src/main/java/com/example/taxi/SignInActivity.java
@@ -2,16 +2,14 @@ package com.example.taxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, SignInActivity.class);
-        startActivity(intent);
-        finish();
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_sign_in);
     }
 }
